4bcd1925b8289cd1ff2df242a1e12b7305550cbb
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-11-20 20:27:28 +0000

    Work on the random generator
    Currently working on genearating 1000 requests with random sector number.

---
+++ diskSchedulingAlgo.cpp
@@ -0,0 +1,18 @@
+//Programmer: Chau Nguyen
+
+#include <iostream>
+
+const int NUMBER_OF_CYLINDERS = 5000;
+const int NUMBER_OF_REQUESTS = 1000;
+
+int main()
+{
+    //Create a random generator. This is pseudo random, not completely random, by the way.
+    //Use current time as seed
+    srand(time(NULL));
+    int num = rand() % ;
+    std::cout << num;
+
+    return 0;
+
+}
