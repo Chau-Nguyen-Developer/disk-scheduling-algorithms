907e245e3c99368dfd71643eb8701debc13c08cf
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-11-22 23:22:04 +0000

    Create first draft for Shortest Seek Time first
    Still work on the logic of the algorithm

--- algorithms.cpp
+++ algorithms.cpp
@@ -3,17 +3,57 @@
 #include <iostream>
 #include <array>
 #include <cmath>
+#include <algorithm>
 #include "constants.h"
 
 
-int FCFS(int head, const std::array<int, NUMBER_OF_REQUESTS>& array)
+int FCFS(int head, const std::vector<int>& vector)
 {
-    int headMovement = std::abs(array[0] - head);
+    int headMovement = std::abs(vector[0] - head);
+
     for (size_t i = 1; i < NUMBER_OF_REQUESTS; ++i)
     {
-        headMovement += std::abs(array[i] - array[i-1]); 
+        headMovement += std::abs(vector[i] - vector[i-1]); 
     }
 
     return headMovement;
-
 } 
+
+int SSTF(int head, std::vector<int> vector)
+{
+    int result = 0;
+    //Find the next location that is closet to head
+    size_t i = 0;
+
+    while(i < NUMBER_OF_REQUESTS)
+    {
+        int current = vector[0];
+        int oldMinDistance = std::abs(current - head);
+        int newMinDistance = 0;
+        size_t nextIndex = 0;
+        bool justDeleted = false;
+        for (size_t j = 0; j< NUMBER_OF_REQUESTS; ++j)
+        {
+            newMinDistance = std::min(oldMinDistance, std::abs(vector[j] - head));
+            if(newMinDistance != oldMinDistance)
+            {
+                //Obtain the next location
+                nextIndex = j;
+            }
+        }
+        result += newMinDistance;
+        //Obtain new head value
+        head = vector[nextIndex];
+        //Delete that entry, so that we don't duplicate calculating it. 
+        vector.erase(vector.begin() + nextIndex);
+
+        
+        //Remove the element at that index
+
+        //may not increment i. may done = true when delete all elements in vector. 
+
+        ++i;
+    } 
+
+    return 0;
+}
--- diskSchedulingAlgo.cpp
+++ diskSchedulingAlgo.cpp
@@ -4,7 +4,9 @@
 #include "algorithms.cpp"
 #include <iostream>
 #include <random>
-#include <array>
+// #include <array>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
@@ -15,7 +17,7 @@ int main()
     //An array to contain the cylinder request
     // int cylinderRequests[NUMBER_OF_REQUESTS]; <-- old style C
 
-    std::array<int, NUMBER_OF_REQUESTS> cylinderRequests;
+    std::vector<int> cylinderRequests;
 
     //Generate a random series of 1,000 cylinder requests
     for (size_t i = 0; i < NUMBER_OF_REQUESTS; ++i)
