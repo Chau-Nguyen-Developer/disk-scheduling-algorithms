fca5e7664f6c8f5beec0a7a3713dc98cb9c9dc6c
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-12-13 02:00:27 +0000

    Working on draft 3 of SSTF algo

--- algorithms.cpp
+++ algorithms.cpp
@@ -32,88 +32,104 @@ int FCFS(int head, const std::vector<int>& myVector)
 }
 
 //SSTF: Shortest Seek Time First
-int SSTF(int head, std::vector<int>& myVector)
-{
-    if (myVector.empty())
-    {
-        std::cout << "Vector is empty." << std::endl;
-        return 0;
-    }
-    std::cout << "Head is " << head << std::endl;
-
-    int headMovement = 0;
-    int min;
-    int indexNextMove = 0;
-    int len = myVector.size(); //this value will change according to time
-    int temp;
-
-    for(size_t i = 0; i < NUMBER_OF_REQUESTS; ++i)
-    {
-         min = std::abs(head - myVector[0]);
-
-        //Identify where the head will move next to
-        for(size_t i = 1; i < len; ++i)
-        {
-            temp = std::abs(head - myVector[i]);
-            if(temp < min)
-            {
-                min = temp;
-                indexNextMove = i;
-            }
-        }
-        std::cout << "Current head: " << head << std::endl;
-        std::cout << "Next position: " << myVector[indexNextMove] << std::endl;
-        
-        //Calculation head movement 
-        headMovement += min;
-
-        //Reset head
-        head = myVector[indexNextMove];
-
-        //remove the element
-        std::swap(myVector[indexNextMove], myVector.back());
-        myVector.pop_back();
-    }
-
-    return headMovement;
-
-}
-
-// int SSTF(int head, std::vector<int> vector)
+// int SSTF(int head, std::vector<int>& myVector)
 // {
-//     int result = 0;
-//     //Find the next location that is closet to head
-//     size_t i = 0;
-
-//     while(i < NUMBER_OF_REQUESTS)
+//     if (myVector.empty())
 //     {
-//         int current = vector[0];
-//         int oldMinDistance = std::abs(current - head);
-//         int newMinDistance = 0;
-//         size_t nextIndex = 0;
-//         bool justDeleted = false;
-//         for (size_t j = 0; j< NUMBER_OF_REQUESTS; ++j)
+//         std::cout << "Vector is empty." << std::endl;
+//         return 0;
+//     }
+//     std::cout << "Head is " << head << std::endl;
+
+//     int headMovement = 0;
+//     int min;
+//     int indexNextMove = 0;
+//     int len = myVector.size(); //this value will change according to time
+//     int temp;
+
+//     for(size_t i = 0; i < NUMBER_OF_REQUESTS; ++i)
+//     {
+//          min = std::abs(head - myVector[0]);
+
+//         //Identify where the head will move next to
+//         for(size_t i = 1; i < len; ++i)
 //         {
-//             newMinDistance = std::min(oldMinDistance, std::abs(vector[j] - head));
-//             if(newMinDistance != oldMinDistance)
+//             temp = std::abs(head - myVector[i]);
+//             if(temp < min)
 //             {
-//                 //Obtain the next location
-//                 nextIndex = j;
+//                 min = temp;
+//                 indexNextMove = i;
 //             }
 //         }
-//         result += newMinDistance;
-//         //Obtain new head value
-//         head = vector[nextIndex];
-//         //Delete that entry, so that we don't duplicate calculating it. 
-//         vector.erase(vector.begin() + nextIndex);
-
+//         std::cout << "Current head: " << head << std::endl;
+//         std::cout << "Next position: " << myVector[indexNextMove] << std::endl;
         
-//         //Remove the element at that index
+//         //Calculation head movement 
+//         headMovement += min;
 
-//         //may not increment i. may done = true when delete all elements in vector. 
+//         //Reset head
+//         head = myVector[indexNextMove];
 
-//         ++i;
-//     } 
+//         //remove the element
+//         std::swap(myVector[indexNextMove], myVector.back());
+//         myVector.pop_back();
+//     }
 
-//     return 0;
-//}
+//     return headMovement;
+
+// }
+int SSTF(int head, std::vector<int>& myVector)
+{
+    int result = 0;
+    //Take head and subtract head to every element to identify which element yields the minimum distance
+    int min = std::abs(head - myVector[0]);
+    int nextIndex = 0;
+    int temp;
+   
+
+    //This is the for loop to indentify which element yields the minimum distance from current head
+    for(size_t i = 1; i < myVector.size(); ++i)
+    {
+        temp = std::abs(myVector[i] - head);
+        if(temp < min)
+        {
+            min = temp;
+            nextIndex = i;
+        }
+    }
+
+    //Now, we got the element and the index that yiels the minimum distance with head
+    result += min;
+    head = myVector[nextIndex];
+
+    //After asigning the value at "nextIndex" to head, delete that element so we don't recalculate it. 
+    std::swap(myVector[nextIndex], myVector.back());
+    myVector.pop_back();
+
+    //Now, do the for loop again
+
+
+    for(size_t i = 1; i < myVector.size(); ++i)
+    {
+        if(myVector.empty())
+        {
+            break;
+        }
+
+        //Take head and subtract head to every element to identify which element yields the minimum distance
+       
+        temp = std::abs(head - myVector[i]);
+        if(temp < min)
+        {
+            min = temp;
+            nextIndex = i;
+        }
+
+        result += min;
+        //Update head
+
+        //Remove that element
+
+        min = std::abs(head - myVector[0]);
+    }
+}
